5fb412aa5c22390c55328292b7ae742658fee822
unknown <Administrator@ABP-HQ7-L074.accessbankplc.com>
2022-03-28 08:06:02 +0100

    swagger docs

--- dist/server.js
+++ dist/server.js
@@ -13,6 +13,11 @@ const app = (0, express_1.default)();
 const port = process.env.PORT || 3001;
 app.use(express_1.default.json({ limit: "50mb" }));
 app.use(express_1.default.static("public"));
+app.use("/docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(undefined, {
+    swaggerOptions: {
+        url: "../swagger.json",
+    },
+}));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -22,11 +27,6 @@ app.use(function (req, res, next) {
     return next();
 });
 app.use((0, morgan_1.default)(":method :url statusCode ===  :status :res[content-length] - :response-time ms"));
-app.use("/docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(undefined, {
-    swaggerOptions: {
-        url: "/swagger.json",
-    },
-}));
 app.use("/", auth_1.default, sms_route_1.default);
 app.listen(port, () => {
     console.log(`subscriber connected to ${port}`);
